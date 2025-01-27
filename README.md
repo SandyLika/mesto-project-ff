f1916f0b50d7836fc5c953a7dcf3278d177c64e1
SandyLika <weerkss@yandex.ru>
2025-01-27 18:20:00 +0300

    pr6c1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/SandyLika/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+// Темплейт?? карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
+const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+const addButton= document.querySelector('.profile__add-butto');
+//  DOM узлы
 
-// @todo: Функция создания карточки
+initialCards.forEach((el) => {    
+  cardsContainer.append(createCard(el, delCard));
+});
 
-// @todo: Функция удаления карточки
+//  Функция создания карточки
+
+function createCard (card, openCB) { //CB-CallBack
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
+  const delButton = cardItem.querySelector('.card__delete-button');
+  delButton.addEventListener('click',() => openCB(cardItem));
+  return cardItem;
+}
+
+// Функция удаления карточки
+
+function delCard (card) {
+  card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+addButton.addEventListener('click', function () {
+  createCard(card,openCB)
+});
